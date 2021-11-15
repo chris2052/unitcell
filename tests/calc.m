53d12a97b4f42592b8b7239e51ff10ffbe305ba3
Christopher Nies <chris-nies@hotmail.de>
2021-11-15 16:42:05 +0100

    compute with mesh

--- tests/calc.m
+++ tests/calc.m
@@ -2,25 +2,32 @@ clearvars
 tic 
 
 %% input parameters
-nodesQ4 = [
-    0, 0;
-    1, 0;
-    1, 1;
-    0, 1];
+% nodesQ4 = [
+%     0, 0;
+%     1, 0;
+%     1, 1;
+%     0, 1];
+% 
+% nodesQ9 = [
+%     0, 0;
+%     1, 0;
+%     1, 1;
+%     0, 1;
+%     0.5, 0;
+%     1, 0.5;
+%     0.5, 1;
+%     0, 0.5;
+%     0.5, 0.5];
+% 
+% nodes = nodesQ9;
+% loading mesh
+beam
 
-nodesQ9 = [
-    0, 0;
-    1, 0;
-    1, 1;
-    0, 1;
-    0.5, 0;
-    1, 0.5;
-    0.5, 1;
-    0, 0.5;
-    0.5, 0.5];
+numEl = size(msh.QUADS9, 1);
 
-nodes = nodesQ9;
-numEl = 1;
+nodPEle = size(msh.QUADS9,2)-1;
+
+conn = msh.QUADS9(:, 1:nodPEle);
 
 %% material properties
 % Youngs Modulus [N/m^2]. For multiple materials use vector: [E1;E2;...]. 
@@ -41,6 +48,9 @@ physics = "strain";
 
 order = 2;
 
+% degree of freedom per element
+dof = 2;
+
 % scheme of the natural coordinate system (order = 2)
 %                       7
 %  (-1,1)    4----------o---------3 (1,1) 
@@ -58,18 +68,18 @@ order = 2;
 %
 %% generate element stiffnes matrix k
 %
-% assembling k using gauss-quadrature
-% for n = 1:order^2
-%     [detJ, invJT] = JacobianQuad(intNodes(n,1), intNodes(n,2), nodes);
-%     [B, N] = b_matrix(intNodes(n,1), intNodes(n,2), invJT, 'q9');
-%     k = k + (t * B' * C * B * detJ) * w(n);
-% end
-parfor n = 1:numEl
+for n = 1:numEl
+    nodes = msh.POS(msh.QUADS9(n,1:nodPEle), :);
     [elementK, elementM] = Element2D(nodes, order, matProp, physics);
     Elements{n}.K = elementK;
     Elements{n}.M = elementM;
+    Elements{n}.DOFs=reshape(repmat(conn(n,1:nodPEle),dof,1)*dof...
+         - repmat((dof-1:-1:0)',1,nodPEle),[],1)';
 end
+%     Elements{i}.DOFs=reshape(repmat(Elements0(i,1:nodPEle),di,1)*di...
+%         - repmat((di-1:-1:0)',1,nodPEle),[],1)';
 
-% [Ksys, Msys]=FastMatrixAssembly(Elements);
+
+[Ksys, Msys]=FastMatrixAssembly(Elements);
 
 toc
