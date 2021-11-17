580c93b07034310e2c449af788e6f49474e5cab1
Christopher Nies <chris-nies@hotmail.de>
2021-11-17 13:55:26 +0100

    drawingfield2d function

--- tests/calc.m
+++ tests/calc.m
@@ -1,5 +1,5 @@
 clearvars
-tic 
+tic
 
 %% input parameters
 % loading mesh
@@ -9,13 +9,13 @@ beam
 numEl = size(msh.QUADS9, 1);
 
 % Nodes per Element
-nodPEle = size(msh.QUADS9,2)-1;
+nodPEle = size(msh.QUADS9, 2) - 1;
 
 %% material properties
 % Matrix material index for PnC=1!
-% For multiple materials use vector: [E1;E2;...]. 
+% For multiple materials use vector: [E1;E2;...].
 
-% Youngs Modulus [N/m^2]. 
+% Youngs Modulus [N/m^2].
 E = 1;
 
 % Poission ratio [-]
@@ -28,7 +28,7 @@ rho = 1;
 t = 1;
 
 % material matrix
-matProp=[E, v, rho, t];
+matProp = [E, v, rho, t];
 
 % (plane) "strain", "stress"
 physics = "strain";
@@ -44,7 +44,7 @@ gDof = dof * msh.nbNod;
 
 % scheme of the natural coordinate system (order = 2)
 %                       7
-%  (-1,1)    4----------o---------3 (1,1) 
+%  (-1,1)    4----------o---------3 (1,1)
 %            |                    |
 %            |   x      x     x   |
 %            |         eta        |
@@ -54,102 +54,58 @@ gDof = dof * msh.nbNod;
 %            |                    |
 %            |   x      x     x   |
 %            |                    |
-%  (-1,-1)   1----------o---------2 (1,-1) 
+%  (-1,-1)   1----------o---------2 (1,-1)
 %                       5
 %
 %% generate element stiffnes matrix k
 %
 parfor n = 1:numEl
     % Connectivity Matrix
-    conn = msh.QUADS9(n,1:nodPEle);
+    conn = msh.QUADS9(n, 1:nodPEle);
     % Node matrix
     nodes = msh.POS(conn, :);
-    % generating locale stiffness/mass matrix 
+    % generating locale stiffness/mass matrix
     [elementK, elementM] = Element2D(nodes, order, matProp, physics);
     Elements{n}.K = elementK;
     Elements{n}.M = elementM;
-    Elements{n}.DOFs = reshape(repmat(conn ,dof,1)*dof...
-         - repmat((dof-1:-1:0)',1,nodPEle),[],1)';
+    Elements{n}.DOFs = reshape(repmat(conn, dof, 1) * dof ...
+        - repmat((dof - 1:-1:0)', 1, nodPEle), [], 1)';
 end
 
-[Ksys, Msys]=FastMatrixAssembly(Elements);
+[Ksys, Msys] = FastMatrixAssembly(Elements);
 
 % boundary conditions, physLine{11} (gmsh) fixed
-uFixed = 2 * unique(msh.LINES3(find(msh.LINES3(:,end)==11), (1:end-1))) - 1;
-vFixed = 2 * unique(msh.LINES3(find(msh.LINES3(:,end)==11), (1:end-1)));
+uFixed = 2 * unique(msh.LINES3(find(msh.LINES3(:, end) == 11), (1:end - 1))) - 1;
+vFixed = 2 * unique(msh.LINES3(find(msh.LINES3(:, end) == 11), (1:end - 1)));
 prescrDof = [uFixed; vFixed];
 
 % forces, physLine{12} (gmsh) line load in v
 load = -1;
 force = zeros(gDof, 1);
-right = 2 * unique(msh.LINES3(find(msh.LINES3(:,end)==12), (1:end-1)));
+right = 2 * unique(msh.LINES3(find(msh.LINES3(:, end) == 12), (1:end - 1)));
 % TODO: approach not correct! height of the beam is relevant!
-force(right) = load/length(right);
+force(right) = load / length(right);
 
 displacements = solution(gDof, prescrDof, Ksys, force);
 
 ux = displacements(1:2:end);
 vy = displacements(2:2:end);
+% wz = zeros(length(vy),1);
 scaleFactor = 0.002;
 
-% drawing mesh
+% drawing color field
 figure;
 
-
-drawingField(msh.POS(:,1:2) + scaleFactor*[ux vy], msh.QUADS9(:,1:9), vy)
+drawingField2D(msh.POS(:, 1:2) + scaleFactor * [ux vy], msh.QUADS9(:, 1:9), vy)
 hold on
 colorbar
 
-for k = 1:size(msh.QUADS9,1)
-    patch(msh.POS(msh.QUADS9(k,1:4),1),msh.POS(msh.QUADS9(k,1:4),2), ...
-        'w','FaceColor','none','LineStyle','--','EdgeColor','k');
+% drawing mesh
+for k = 1:size(msh.QUADS9, 1)
+    patch(msh.POS(msh.QUADS9(k, 1:4), 1), msh.POS(msh.QUADS9(k, 1:4), 2), ...
+        'w', 'FaceColor', 'none', 'LineStyle', '--', 'EdgeColor', 'k');
 end
 
 axis equal
 
 toc
-
-function drawingField(nodes,conn,field)
-  
-if ( size(field) == size(conn) )
-  elementalField=1;
-else
-  elementalField=0;
-end
-
-if (size(nodes,2) < 3)
-   for c=size(nodes,2)+1:3
-      nodes(:,c)=[zeros(size(nodes,1),1)];
-   end
-end
-
-holdState=ishold;
-hold on
-
-% Q9 elements
-%   ord=[1,5,2,6,3,7,4,8,1];
-  ord=[1,2,3,4,1];
-
-for e=1:size(conn,1)
-  
-   xpt=nodes(conn(e,ord),1);
-   ypt=nodes(conn(e,ord),2);      
-   zpt=nodes(conn(e,ord),3);
-   
-   if ( elementalField )
-     fpt=field(e,ord);
-   else
-     fpt=field(conn(e,ord));
-   end
-   
-   fill3(xpt,ypt,zpt,fpt)
-end
-
-shading interp
-axis equal
-      
-if ( ~holdState )
-  hold off
-end
-end
-
