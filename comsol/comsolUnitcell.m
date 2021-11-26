0cfd4989e41b5a849dd3dc16cb41f19daab3ad98
Christopher Nies <chris-nies@hotmail.de>
2021-11-26 12:40:38 +0100

    evaluate comsol model and plot

---
+++ comsol/comsolUnitcell.m
@@ -0,0 +1,38 @@
+clearvars
+close all
+
+model = mphload('unitCell_validation');
+
+model.param.set('L1', '0.1[m]', 'Periodic cell height');
+model.param.set('L2', '0.1[m]', 'Periodic cell length');
+
+model.param.set('Rout', '(.075/2) [m]', 'Size of outer material');
+model.param.set('Rin', '(.02/2) [m]', 'Radius of inner material');
+
+model.param.set('numEigs', '6', 'Number of Eigenfruequencies/Bands');
+
+model.param.set('E1', '0.93e6[N/m^2]', 'youngs modulus');
+model.param.set('Rho1', '1250[kg/m^3]', 'desity');
+model.param.set('Poisson1', '0.45', 'poissons ratio');
+
+model.param.set('E2', '2.1e11[N/m^2]', 'outer mat2');
+model.param.set('Rho2', '7850[kg/m^3]', 'outer mat2');
+model.param.set('Poisson2', '0.3', 'outer mat2');
+
+model.param.set('E3', '2.1e11[N/m^2]', 'inner mat3');
+model.param.set('Rho3', '2300[kg/m^3]', 'inner mat3');
+model.param.set('Poisson3', '0.3', 'inner mat3');
+
+model.study('std2').run;
+
+%% plotting
+% mphplot(model, 'pg7');
+% 
+% % Create labels
+% ylabel('Frequenz [Hz]','FontSize',12,'Interpreter','latex');
+% xlabel(' ','FontSize',12,'Interpreter','latex');
+% 
+% box(axes1,'on');
+% grid(axes1,'on');
+% % Set the remaining axes properties
+% set(axes1,'FontSize',12,'TickLabelInterpreter','latex');
