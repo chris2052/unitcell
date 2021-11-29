60bf2936248d3e49d52823124957f009d53787ba
Christopher Nies <chris-nies@hotmail.de>
2021-11-29 13:21:06 +0100

    fixing LaTeX font

--- DispersionCurves/plotDispersion.m
+++ DispersionCurves/plotDispersion.m
@@ -6,8 +6,8 @@ function plotDispersion(fBand, deltaKx, deltaKy, kxy0, BasisVec, Font, ...
 Xptick = numel((0:deltaKx:pi)');
 Mptick = Xptick + numel(deltaKy:deltaKy:pi);
 
-LightGrey = [0.8, 0.8, 0.8];
-set(gcf, 'Position', [0, 0, 1920, 1080])
+LightGrey = [0.7, 0.7, 0.7];
+% set(gcf, 'Position', [0, 0, 1920, 1080])
 hold on
 fBand = abs(fBand);
 FreqBands = plot(kxy0, fBand, 'k', 'LineWidth', LineLineWidth);
@@ -20,17 +20,19 @@ indBand = find(fBandDelta > 10);
 
 for bgi = 1:size(indBand, 2)
 	Bandgaps = fill([0 max(kxy0) max(kxy0) 0], [fBandMax(indBand(bgi)) fBandMax(indBand(bgi)) fBandMin(indBand(bgi)) fBandMin(indBand(bgi))], LightGrey);
-	text(0.25, (fBandMin(indBand(bgi)) + fBandMax(indBand(bgi))) / 2, [num2str(bgi), '. Bandlücke: ', num2str(fBandMax(indBand(bgi)), '%.0f'), ' - ', num2str(fBandMin(indBand(bgi)), '%.0f'), ' Hz'], 'fontsize', 8, 'fontName', Font);
+	text(10, (fBandMin(indBand(bgi)) + fBandMax(indBand(bgi))) / 2, [num2str(bgi), '. Bandl{\"u}cke: ', num2str(fBandMax(indBand(bgi)), '%.0f'), ' - ', num2str(fBandMin(indBand(bgi)), '%.0f'), ' Hz'], 'FontSize', FontSize, 'fontName', Font, 'interpreter', 'latex');
 end
 
 box on
 set(gca, 'Layer', 'top')
+set(gca, 'TickLabelInterpreter', 'latex', 'FontSize', FontSize)
 pbaspect([1 1 1]);
-xlabel('Wellenvektor')
+xlabel('Wellenvektor', 'interpreter', 'latex')
 ylabel('$f$ [Hz]', 'interpreter', 'latex')
 figureHandle = gcf;
-set(findall(figureHandle, 'type', 'text'), 'fontSize', FontSize, 'fontWeight', 'normal', 'fontName', Font)
-set(findall(figureHandle, 'type', 'axes'), 'fontsize', FontSize, 'fontWeight', 'normal', 'fontName', Font)
+% set(findall(figureHandle, 'type', 'text'), 'fontSize', FontSize, 'fontWeight', 'normal', 'fontName', Font)
+% set(findall(figureHandle, 'type', 'axes'), 'fontsize', FontSize, 'fontWeight', 'normal', 'fontName', Font)
+
 
 if size(BasisVec, 1) == 1
 	axis([0 max(kxy0) 0 1.0 * max(max(abs(fBand)))]);
@@ -40,7 +42,7 @@ if size(BasisVec, 1) == 1
 else
 	axis([0 max(kxy0) 0 1.0 * max(max(abs(fBand)))]);
 	xticks([0, Xptick, Mptick, max(kxy0)])
-	xticklabels({'\Gamma', 'X', 'M', '\Gamma'})
+	xticklabels({'$\Gamma$', 'X', 'M', '$\Gamma$'})
 	% set(gca,'ytick',[])
 	% set(gca,'yticklabel',[])
 end
