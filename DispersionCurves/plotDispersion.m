4972ccc7fe8f4d171c4a15394b399f5466dfcdf2
Christopher Nies <chris-nies@hotmail.de>
2022-01-05 11:18:21 +0100

    removed gnuplot idea

--- DispersionCurves/plotDispersion.m
+++ DispersionCurves/plotDispersion.m
@@ -1,5 +1,5 @@
 function plotDispersion(fBand, deltaKx, deltaKy, kxy0, BasisVec, ...
-    FontSize, LineLineWidth, gnuplot)
+    FontSize, LineLineWidth)
     %PLOTDISPERSION Summary of this function goes here
     %   Detailed explanation goes here
 
@@ -18,48 +18,38 @@ fBandMax = fBandMax(1:end - 1);
 fBandDelta = fBandMin - fBandMax;
 indBand = find(fBandDelta > 10);
 
-if ~gnuplot
-    % plotting with MATLAB
-    hold on
-    FreqBands = plot(kxy0, fBand, 'k', 'LineWidth', LineLineWidth);
-    
-    for bgi = 1:size(indBand, 2)
-	    Bandgaps = fill([0 max(kxy0) max(kxy0) 0], [fBandMax(indBand(bgi)) fBandMax(indBand(bgi)) fBandMin(indBand(bgi)) fBandMin(indBand(bgi))], LightGrey);
-	    text(10, (fBandMin(indBand(bgi)) + fBandMax(indBand(bgi))) / 2, [num2str(bgi), '. Bandl{\"u}cke: ', num2str(fBandMax(indBand(bgi)), '%.0f'), ' - ', num2str(fBandMin(indBand(bgi)), '%.0f'), ' Hz'], 'FontSize', FontSize, 'interpreter', 'latex');
-    end
-    
-    box on
-    set(gca, 'Layer', 'top')
-    set(gca, 'TickLabelInterpreter', 'latex', 'FontSize', FontSize)
-    pbaspect([1 1 1]);
-    xlabel('Wellenvektor', 'interpreter', 'latex', 'FontSize', FontSize)
-    ylabel('$f$ [Hz]', 'interpreter', 'latex', 'FontSize', FontSize)
-    % figureHandle = gcf;
-    % set(findall(figureHandle, 'type', 'text'), 'fontSize', FontSize, 'fontWeight', 'normal', 'fontName', Font)
-    % set(findall(figureHandle, 'type', 'axes'), 'fontsize', FontSize, 'fontWeight', 'normal', 'fontName', Font)    
-    
-    if size(BasisVec, 1) == 1
-	    axis([0 max(kxy0) 0 1.0 * max(max(abs(fBand)))]);
-	    ax = gca;
-	    ax.XTick = [0, max(kxy0)];
-	    xticklabels({'$\Gamma$', 'X'})
-    else
-	    axis([0 max(kxy0) 0 1.0 * max(max(abs(fBand)))]);
-	    xticks([0, Xptick, Mptick, max(kxy0)])
-	    xticklabels({'$\Gamma$', 'X', 'M', '$\Gamma$'})
-	    % set(gca,'ytick',[])
-	    % set(gca,'yticklabel',[])
-    end
-    
-    hold off
+% plotting with MATLAB
+hold on
+FreqBands = plot(kxy0, fBand, 'k', 'LineWidth', LineLineWidth);
 
-else
-    % plotting with gnuplot
-    %
-    %TODO gnuplot plotting functions
-    % max(kxy0) = 429
-    % plot [0:429] [] for [j=2:7] 'test.txt' using 1:j with lines
-
+for bgi = 1:size(indBand, 2)
+    Bandgaps = fill([0 max(kxy0) max(kxy0) 0], [fBandMax(indBand(bgi)) fBandMax(indBand(bgi)) fBandMin(indBand(bgi)) fBandMin(indBand(bgi))], LightGrey);
+    text(10, (fBandMin(indBand(bgi)) + fBandMax(indBand(bgi))) / 2, [num2str(bgi), '. Bandl{\"u}cke: ', num2str(fBandMax(indBand(bgi)), '%.0f'), ' - ', num2str(fBandMin(indBand(bgi)), '%.0f'), ' Hz'], 'FontSize', FontSize, 'interpreter', 'latex');
 end
+
+box on
+set(gca, 'Layer', 'top')
+set(gca, 'TickLabelInterpreter', 'latex', 'FontSize', FontSize)
+pbaspect([1 1 1]);
+xlabel('Wellenvektor', 'interpreter', 'latex', 'FontSize', FontSize)
+ylabel('$f$ [Hz]', 'interpreter', 'latex', 'FontSize', FontSize)
+% figureHandle = gcf;
+% set(findall(figureHandle, 'type', 'text'), 'fontSize', FontSize, 'fontWeight', 'normal', 'fontName', Font)
+% set(findall(figureHandle, 'type', 'axes'), 'fontsize', FontSize, 'fontWeight', 'normal', 'fontName', Font)    
+
+if size(BasisVec, 1) == 1
+    axis([0 max(kxy0) 0 1.0 * max(max(abs(fBand)))]);
+    ax = gca;
+    ax.XTick = [0, max(kxy0)];
+    xticklabels({'$\Gamma$', 'X'})
+else
+    axis([0 max(kxy0) 0 1.0 * max(max(abs(fBand)))]);
+    xticks([0, Xptick, Mptick, max(kxy0)])
+    xticklabels({'$\Gamma$', 'X', 'M', '$\Gamma$'})
+    % set(gca,'ytick',[])
+    % set(gca,'yticklabel',[])
+end
+
+hold off
 
 end
