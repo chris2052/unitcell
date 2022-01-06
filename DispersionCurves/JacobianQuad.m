a662af775a373aa67c8d618bf4b934f64512cd4c
Christopher Nies <chris-nies@hotmail.de>
2022-01-06 10:01:35 +0100

    cleaned up code

--- DispersionCurves/JacobianQuad.m
+++ DispersionCurves/JacobianQuad.m
@@ -21,12 +21,6 @@ function [detJ, invJT] = JacobianQuad(xi, eta, nodes)
     y3 = yy(3);
     y4 = yy(4);
 
-%     J = [(eta*x1*xi*(eta - 1))/4 + (eta*x2*xi*(eta - 1))/4 + (eta*x3*xi*(eta + 1))/4 + (eta*x4*xi*(eta + 1))/4 + (eta*x1*(eta - 1)*(xi - 1))/4 + (eta*x2*(eta - 1)*(xi + 1))/4 + (eta*x3*(eta + 1)*(xi + 1))/4 + (eta*x4*(eta + 1)*(xi - 1))/4, (eta*x1*xi*(xi - 1))/4 + (eta*x2*xi*(xi + 1))/4 + (eta*x3*xi*(xi + 1))/4 + (eta*x4*xi*(xi - 1))/4 + (x1*xi*(eta - 1)*(xi - 1))/4 + (x2*xi*(eta - 1)*(xi + 1))/4 + (x3*xi*(eta + 1)*(xi + 1))/4 + (x4*xi*(eta + 1)*(xi - 1))/4; 
-%     (eta*xi*y1*(eta - 1))/4 + (eta*xi*y2*(eta - 1))/4 + (eta*xi*y3*(eta + 1))/4 + (eta*xi*y4*(eta + 1))/4 + (eta*y1*(eta - 1)*(xi - 1))/4 + (eta*y2*(eta - 1)*(xi + 1))/4 + (eta*y3*(eta + 1)*(xi + 1))/4 + (eta*y4*(eta + 1)*(xi - 1))/4, (eta*xi*y1*(xi - 1))/4 + (eta*xi*y2*(xi + 1))/4 + (eta*xi*y3*(xi + 1))/4 + (eta*xi*y4*(xi - 1))/4 + (xi*y1*(eta - 1)*(xi - 1))/4 + (xi*y2*(eta - 1)*(xi + 1))/4 + (xi*y3*(eta + 1)*(xi + 1))/4 + (xi*y4*(eta + 1)*(xi - 1))/4];
-% 
-%     detJ = det(J);
-%     invJT = inv(J)';
-%     
     detJ = 1/8*((x1*y2) - (x2*y1) - (x1*y4) + (x2*y3) - (x3*y2) + (x4*y1) ...
         + (x3*y4) - (x4*y3) - (eta*x1*y2) + (eta*x2*y1) + (eta*x1*y3) ...
         - (eta*x3*y1) - (eta*x2*y4) + (eta*x4*y2) + (eta*x3*y4) - (eta*x4*y3) ...
