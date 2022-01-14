99c46504abc24a768e130151b40166292ae61aef
Christopher Nies <chris-nies@hotmail.de>
2022-01-14 16:44:28 +0100

    combined all in one function

--- DispersionCurves/drawingMesh2D.m
+++ DispersionCurves/drawingMesh2D.m
@@ -3,6 +3,15 @@ function drawingMesh2D(nodesCornerX, nodesCornerY, FaceColor, ...
 %DRAWINGMESH2D Summary of this function goes here
 %   Detailed explanation goes here
 
+meshFigure = figure;
+meshFigure.Units = 'centimeters';
+meshFigure.Position = [20, 8, 10, 10];
+
+meshAxis = axes;
+meshAxis.Position = [0, 0, 1, 1];
+meshAxis.XColor = 'none';
+meshAxis.YColor = 'none';
+
 for k = 1:size(nodesCornerX, 1)
     patch(nodesCornerX(k, :), nodesCornerY(k, :), 'w', 'FaceColor', FaceColor, ...
         'LineStyle', LineStyle, 'EdgeColor', EdgeColor);
