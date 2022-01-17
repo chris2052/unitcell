007b7c8c89e8ab94100e31da57034ace6164e08d
Christopher Nies <chris-nies@hotmail.de>
2022-01-17 10:29:36 +0100

    function to calibrate plot dimensions

---
+++ DispersionCurves/plotDimensions.m
@@ -0,0 +1,25 @@
+function plotDimensions(fighandle, axhandle, xDim, yDim, factor, angle)
+%PLOTDIMENSIONS Summary of this function goes here
+%   Detailed explanation goes here
+
+fighandle.Units = 'centimeters';
+fighandle.Position = [35, 8, xDim, yDim];
+
+fighandle.PaperUnits = 'centimeters';
+fighandle.PaperPositionMode = 'manual';
+fighandle.PaperSize = [xDim, yDim];
+fighandle.PaperPosition = [0, 0, xDim, yDim];
+fighandle.Renderer = 'painters';
+
+% centering axis
+border = (1 - factor)/2;
+axhandle.Position = [border, border, factor, factor ];
+
+% setting angle for xticks
+if nargin < 6
+    angle = 0;
+end
+
+xtickangle(axhandle, angle);
+
+end
