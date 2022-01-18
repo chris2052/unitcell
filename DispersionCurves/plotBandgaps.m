286d09d0dda46c9c40dd7fb478a97441236719af
Christopher Nies <chris-nies@hotmail.de>
2022-01-18 17:05:30 +0100

    functional barPlot

--- DispersionCurves/plotBandgaps.m
+++ DispersionCurves/plotBandgaps.m
@@ -1,9 +1,9 @@
 function plotBandgaps(bandgaps, label, position, color)
-%BAND Summary of this function goes here
+%PLOTBANDGAPS Summary of this function goes here
 %   Detailed explanation goes here
 
 fig = figure;
-ax = axes;
+ax = axes(fig);
 
 width = 0.6;
 
@@ -29,7 +29,7 @@ for n = 1:size(bandgaps, 1)
     for m = 2:2:size(row, 2)
         if row(m) > 0
         rectangle('Position', [n + x1, row(m-1), x2, row(m) - row(m-1)],...
-            'FaceColor', color, 'EdgeColor', color);
+            'FaceColor', color, 'EdgeColor', 'k');
         hold on
         end
         sumRow = sumRow + row(m) - row(m-1);
@@ -42,14 +42,7 @@ hold off
 grid(ax, 'on');
 grid(ax, 'minor');
 
-fig.Units = 'centimeters';
-plotDimX = 9;
-plotDimY = 7;
-
-fig.Position = [35, 8, plotDimX, plotDimY];
-ax.Position = [.15, .15, .7, .7];
-
-% axis(ax, [10, 50, 5000, inf]);
+axis(ax, [0, inf, 0, inf]);
 
 xlabel(ax, label);
 ylabel(ax, 'Frequenz $f$ [Hz]')
