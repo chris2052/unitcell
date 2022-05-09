4ec948dbc908b97fd4ed9ba9b5ed9c954f9ac397
Christopher Nies <chris-nies@hotmail.de>
2022-05-09 18:02:22 +0200

    plot function for complex band structures

---
+++ ComplexBandstructure/plotDispersionCompl.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = plotDispersionCompl(inputArg1,inputArg2)
+%PLOTDISPERSIONCOMPL Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
