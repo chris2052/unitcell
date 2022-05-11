5125de7c023572eb0a499bbf320416b9b61d43f9
Christopher Nies <chris-nies@hotmail.de>
2022-05-11 13:15:01 +0200

    plot routine for fullscreen

--- ComplexBandstructure/ComplBandStructQUAD.m
+++ ComplexBandstructure/ComplBandStructQUAD.m
@@ -270,7 +270,7 @@ omegSCMG = repmat((0.1:dOmegC:(ceil(OmegC/dOmegC))*dOmegC + 0.1)/(2*pi),size(kxS
 % Set default line width of all lines within plots
 % LineLineWidth=1;
 % Set marker size for complex band structure
-MarkerSize1 = 5;
+MarkerSize1 = 2;
  
 % set(0, 'DefaultAxesLineWidth', AxesLineWidth);
 % set(0, 'DefaultLineLineWidth', LineLineWidth);
@@ -278,9 +278,17 @@ MarkerSize1 = 5;
 DRed=[0.70 0.2 0.2];                   %Dark Red
 DBlue=[.2 .2 0.7];                     %Dark Blue 
 
-ComplBandFig = figure;
+% close figure, if it already exists (to update)
+if isgraphics(ComplBandFig)
+    close(ComplBandFig)
+end
+
+% show figure in fullscreen
+ComplBandFig = figure('units','normalized','outerposition',[0 0 1 1]);
+
+% set dimensions for publication
 ComplBandFig.Units = 'centimeters';
-ComplBandFig.Position = [10, 10, 15, 10];
+ComplBandFig.Position = [70, 10, 15, 10];
 
 ComplBandReAx = axes(ComplBandFig);
 ComplBandReAx.Position = [0.1 0.1 0.5 0.8];
@@ -290,7 +298,7 @@ ComplBandReAx.Box = 'on';
 % tilpltBG = tiledlayout(1,2,'TileSpacing','none');
 % 
 % nexttile(tilpltBG)
-..........................
+% ..........................
 
 hold(ComplBandReAx, 'on')
 
@@ -313,16 +321,16 @@ plot(ComplBandReAx, -PRekxSCMG(round(PRekxSCMG,3)>0&round(PRekxSCMG,3)~=round(pi
 plot(ComplBandReAx, CRekxSCGX(round(CRekxSCGX,3)>0&round(CRekxSCGX,3)~=round(pi,3))/pi, ...
     omegSCGX(round(CRekxSCGX,3)>0&round(CRekxSCGX,3)~=round(pi,3)),'o', ...
     'MarkerSize',MarkerSize1,'MarkerFaceColor',DBlue,'MarkerEdgeColor',DBlue);
-
-% CompFreqBandsCRe2
-plot(ComplBandReAx, CRekxSCXM(round(CRekxSCXM,3)>0&round(CRekxSCXM,3)~=round(pi,3))/pi+1, ...
-    omegSCXM(round(CRekxSCXM,3)>0&round(CRekxSCXM,3)~=round(pi,3)),'o', ...
-    'MarkerSize',MarkerSize1,'MarkerFaceColor',DBlue,'MarkerEdgeColor',DBlue);
-
-% CompFreqBandsCRe3
-plot(ComplBandReAx, -CRekxSCMG(round(CRekxSCMG,3)>0&round(CRekxSCMG,3)~=round(pi,3))/pi+3, ...
-    omegSCMG(round(CRekxSCMG,3)>0&round(CRekxSCMG,3)~=round(pi,3)),'o', ...
-    'MarkerSize',MarkerSize1,'MarkerFaceColor',DBlue,'MarkerEdgeColor',DBlue);
+% 
+% % CompFreqBandsCRe2
+% plot(ComplBandReAx, CRekxSCXM(round(CRekxSCXM,3)>0&round(CRekxSCXM,3)~=round(pi,3))/pi+1, ...
+%     omegSCXM(round(CRekxSCXM,3)>0&round(CRekxSCXM,3)~=round(pi,3)),'o', ...
+%     'MarkerSize',MarkerSize1,'MarkerFaceColor',DBlue,'MarkerEdgeColor',DBlue);
+% 
+% % CompFreqBandsCRe3
+% plot(ComplBandReAx, -CRekxSCMG(round(CRekxSCMG,3)>0&round(CRekxSCMG,3)~=round(pi,3))/pi+3, ...
+%     omegSCMG(round(CRekxSCMG,3)>0&round(CRekxSCMG,3)~=round(pi,3)),'o', ...
+%     'MarkerSize',MarkerSize1,'MarkerFaceColor',DBlue,'MarkerEdgeColor',DBlue);
 
 axis(ComplBandReAx, [0 3 0 maxf]);
 xticks(ComplBandReAx, 0:1:3)% create tick marks at 1/4 multiples of pi
