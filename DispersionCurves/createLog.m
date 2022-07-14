226f01e89c11c92ff4bbd8ae210f77af66e4de16
Christopher Nies <chris-nies@hotmail.de>
2022-07-14 14:02:11 +0200

    not working for complex, no need to fix

--- DispersionCurves/createLog.m
+++ DispersionCurves/createLog.m
@@ -1,12 +1,12 @@
-function createLog(log, counter,l1, l2, rIn, rOut, matProp, matName)
+function createLog(log, counter, l1, l2, dInclusion, tCoating, matProp, matName)
 %CREATELOG write calculation information in a *.txt file called `fileName`
 %   Detailed explanation goes here
 
 %log = fopen([fileName, '.txt'], 'w');
 fprintf(log, 'counter: %d\n%s\n', counter, datestr(datetime('now')));
 fprintf(log, 'l1 x l2 = %5.3f x %5.3f [m]\n', l1, l2);
-fprintf(log, 'rOut = %5.3f [m]\t\t', rOut);
-fprintf(log, 'rIn = %5.3f [m]\n\n', rIn);
+fprintf(log, 'tCoating = %5.3f [m]\t\t', tCoating);
+fprintf(log, 'dInclusion = %5.3f [m]\n\n', dInclusion);
 fprintf(log, '%5s %12s %12s %9s %12s %9s \n\n','Num', 'Mat', 'E [N/m2]', ...
     'v [-]', 'rho [kg/m3]', 't [m]');
 for k = 1:size(matProp, 1)
